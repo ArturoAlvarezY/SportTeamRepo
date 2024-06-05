81b356b4cd93799a06ef7816af8707a829dbdcf2
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-05 16:40:55 +0200

    Finished the comenzar button of team.html that when giving click makes us validate the amount of teams if the teams are between 16 and 32, if it is not thus it jumps us an alert The number of teams must be between 16 and 32. If it fulfills that function the following thing is to shuffle the teams and it saves them in an array that does not overwrite the previous one. Then it divides the shuffled teams in groups of 4 that in turn fills us to the next page, named ranking, that fills the tables with those teams already shuffled. Created ranking.html with the same from page 3 (minus the buttons) to make that table work. WARNINGgit merge develop! the page ranking.html is not done. We still need to import the app.js stuff too.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -158,3 +158,83 @@ window.addEventListener('load', () => {
     check()
     navBar()
 });
+
+
+
+
+// Si los equipos estan entre 16 y 32 nos baraja los equipos y nos redirige hasta la pagina del ranking
+// Si no estan entre esas cantidades de equipos nos salta el mensaje 'El número de equipos debe ser entre 16 y 32.'
+// con ([...array]) asegura que no modifica el array original del localStorage
+// Llama a la función para dividir en grupos
+// Guarda los grupos en localStorage para usarlos en la otra página
+// Llama a la función para llenar la tabla con los equipos barajados
+function redirigirBarajear() {
+    let myArray = JSON.parse(localStorage.getItem('myArray'));
+    if (validarCantidadEquipos(myArray)) {
+        const bArray = barajarEquipos([...myArray]);
+        localStorage.setItem('barajadoArray', JSON.stringify(bArray));
+        const grupos = dividirEquiposEnGrupos(bArray); 
+        localStorage.setItem('gruposArray', JSON.stringify(grupos)); 
+        llenarTabla(bArray);
+        window.location.href = "http://127.0.0.1:5500/public/pages/ranking.html"
+    } else {
+        alert('El número de equipos debe ser entre 16 y 32.');
+    }
+}
+
+function validarCantidadEquipos(myArray) {
+    let led = myArray.length
+    if (myArray.length >= 16 && myArray.length <= 32)
+        return true
+    else return false
+}
+
+function barajarEquipos(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function dividirEquiposEnGrupos(array) {
+    const barajadosArray = barajarEquipos([...array]);
+    const numEquipos = barajadosArray.length;
+    const numGrupos = Math.ceil(numEquipos / 4); 
+    const grupos = [];
+
+    for (let i = 0; i < numGrupos; i++) {
+        grupos.push(barajadosArray.slice(i * 4, (i + 1) * 4));
+    }
+
+    return grupos; 
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const barajadoArray = JSON.parse(localStorage.getItem('barajadoArray'));
+
+    if (barajadoArray && Array.isArray(barajadoArray)) {
+        llenarTabla(barajadoArray);
+    } else {
+        alert('No se encontraron equipos barajados en localStorage.');
+    }
+});
+
+function llenarTabla(equipos) {
+    const tableBody = document.querySelector('#rankingTable');
+
+    equipos.forEach((equipo, index) => {
+        const row = document.createElement('tr');
+
+        const cellPosicion = document.createElement('td');
+        cellPosicion.textContent = equipo[0]
+        row.appendChild(cellPosicion);
+
+        const cellEquipo = document.createElement('td');
+        cellEquipo.textContent = equipo[1];
+        row.appendChild(cellEquipo);
+
+        tableBody.appendChild(row);
+
+    });
+}
