696f87b3af183fdcbcb8eb732b88debbbb782e64
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-06 12:48:37 +0200

    modified some small changes in team.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -99,25 +99,19 @@ function llenarTabla() {
     if (localStorage.getItem('myArray') !== null) {
         let tab = JSON.parse(localStorage.getItem('myArray'));
         let temp = '';
-        tab.forEach((element, index) => {
+        tab.forEach(element => {
             temp = temp + `<tr>
                                 <td>${element[0]}</td>
                                 <td>${element[1]}</td>
-                                <td>
-                                <button onclick="edit(${index})">Editar</button>
-                                <button onclick="delete(${index})">Borrar</button>
-                                </td>                              
-                            </tr>`;
-
+                                <td></td>
+                            </tr>
+                        `;
         });
-
-        
         tabla.innerHTML = `
             <tr class="destacado">
                 <td>Bandera</td>
                 <td>Nombre</td>
                 <td></td>
-               
             </tr>
             ${temp}
         `;
@@ -141,51 +135,30 @@ function anadir() {
     if (localStorage.getItem('myArray') !== null) {
         let myArray = JSON.parse(localStorage.getItem('myArray'));
         let nombre = document.getElementById('nombre').value;
-
         let bandera = document.getElementById('bandera').value;
-        let editar = document.getElementById('editarBtn').value;
-        let borrar = document.getElementById('borrarBtn').value;
-        myArray.push([bandera,nombre]);
+        myArray.push([bandera, nombre]);
         localStorage.setItem('myArray', JSON.stringify(myArray));
         llenarTabla();
     } else {
         let myArray = [];
         let nombre = document.getElementById('nombre').value;
         let bandera = document.getElementById('bandera').value;
-        let editar = document.getElementById('editarBtn').value;
-        let borrar = document.getElementById('borrarBtn').value;
-        myArray.push([bandera,nombre]);
+        myArray.push([bandera, nombre]);
         localStorage.setItem('myArray', JSON.stringify(myArray));
         llenarTabla();
     }
     document.getElementById('nombre').value = '';
-    document.getElementById('bandera').value = ''; 
-    let editar = document.getElementById('editarBtn').value = 'editar'
-    let borrar = document.getElementById('borrarBtn').value = 'borrar'
+    document.getElementById('bandera').value = '';
 };
 
-function edit(index) { 
-    let tab = JSON.parse(localStorage.getItem('myArray'));
-     const nuevoNombre = prompt('Ingrese el nuevo nombre para el equipo:', tab[index][1]);
-     if (nuevoNombre) { 
-    tab[index][1] = nuevoNombre.trim(); 
-    localStorage.setItem('myArray', JSON.stringify(tab)); 
-    llenarTabla(); 
-    }
-     } 
-function deleteElement(index) { 
-    let tab = JSON.parse(localStorage.getItem('myArray'));
-     if (confirm('¿Estás seguro de que quieres eliminar este equipo?')) { 
-    tab.splice(index, 1); 
-    localStorage.setItem('myArray', JSON.stringify(tab)); llenarTabla();
-     } 
-    } 
+
 
 window.addEventListener('load', () => {
     //llenarTabla()
     check()
     navBar()
 });
+
 
 
 
